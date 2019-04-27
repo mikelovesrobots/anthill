cbe82fd6a1b08957780aec368d06a0f0299e916d
Mike Judge <mikelovesrobots@gmail.com>
2019-04-26 21:51:51 -0700

    Add controlling scripts for the left and right halves of the lever

---
+++ app/Assets/Scripts/LeverLiftController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverLiftController : MonoBehaviour
+{
+  public string AxisName = "VerticalLeft";
+  public int MinBounds = -4;
+  public int MaxBounds = 4;
+  public float Speed = 2.0f;
+  public Rigidbody2D Rigidbody2D;
+
+  void Update()
+  {
+    var delta = -Input.GetAxis(AxisName) * Speed * Time.deltaTime;
+    var delta2D = new Vector2(0, delta);
+    var desiredPosition = Rigidbody2D.position + delta2D;
+    if (desiredPosition.y < MinBounds)
+    {
+      desiredPosition.y = MinBounds;
+    }
+    if (desiredPosition.y > MaxBounds)
+    {
+      desiredPosition.y = MaxBounds;
+    }
+
+    Rigidbody2D.MovePosition(desiredPosition);
+  }
+}

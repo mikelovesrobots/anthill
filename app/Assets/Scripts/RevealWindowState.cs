8a36566f0c9b37ac1c7556038d48d02093796c7d
Mike Judge <mikelovesrobots@gmail.com>
2019-04-28 13:41:50 -0700

    Design some windows

---
+++ app/Assets/Scripts/RevealWindowState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pixelplacement;
+
+public class RevealWindowState : State
+{
+  public ScreensController ScreensController;
+  public string Name;
+
+  void OnEnable()
+  {
+    ScreensController.Reveal(Name);
+  }
+}
---
+++ app/Assets/Scripts/ScreensController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pixelplacement;
+
+public class ScreensController : MonoBehaviour
+{
+  public Transform[] Screens;
+  public Vector3 OnscreenPosition = Vector3.zero;
+  public Vector3 OffscreenPosition;
+  public float AnimationTime = 1f;
+  public AnimationCurve AnimationCurve;
+
+  private void Start()
+  {
+    foreach (var screen in Screens)
+    {
+      screen.gameObject.SetActive(true);
+      screen.transform.localPosition = OffscreenPosition;
+    }
+  }
+
+  public void Reveal(string name)
+  {
+    foreach (var screen in Screens)
+    {
+      if (screen.gameObject.name == name)
+      {
+        Tween.LocalPosition(screen, OnscreenPosition, AnimationTime, 0f, AnimationCurve);
+      }
+      else
+      {
+        Tween.LocalPosition(screen, OffscreenPosition, AnimationTime, AnimationTime, AnimationCurve);
+      }
+    }
+  }
+}

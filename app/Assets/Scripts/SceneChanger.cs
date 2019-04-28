a1149d0dac2402e1e3bf88de34746cf12d76044d
Mike Judge <mikelovesrobots@gmail.com>
2019-04-27 21:51:57 -0700

    Add a generic retry scene

---
+++ app/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+  public string sceneName = "Game";
+
+  public void ChangeScene()
+  {
+    SceneManager.LoadScene(sceneName);
+  }
+}

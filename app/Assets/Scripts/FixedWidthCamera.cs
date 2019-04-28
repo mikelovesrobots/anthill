328934e62f6fae14edbe289db7fd31e1c61dbb2c
Mike Judge <mikelovesrobots@gmail.com>
2019-04-28 14:54:57 -0700

    Fix screen resolution woes

---
+++ app/Assets/Scripts/FixedWidthCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class FixedWidthCamera : MonoBehaviour
+{
+  public float DesiredWidthInUnits = 16f;
+
+  private float size;
+  private float ratio;
+  private float screenHeight;
+
+  void Awake()
+  {
+    var camera = GetComponent<Camera>();
+    camera.orthographicSize = DesiredWidthInUnits * Screen.height / Screen.width / 2;
+  }
+}
